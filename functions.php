05aba75259a62aea2a6ca05d44f6a42c05bd47e4
GUShap <shapira91@gmail.com>
2024-10-18 01:22:07 +0300

    menus api update

--- functions.php
+++ functions.php
@@ -1,6 +1,9 @@
-<?php 
+<?php
 
-if(!defined('ABSPATH')) exit;
+if (!defined('ABSPATH'))
+    exit;
+
+define('THEME_DIR', get_stylesheet_directory_uri());
 
 // support menu
 add_theme_support('menus');
@@ -10,3 +13,7 @@ add_filter('use_block_editor_for_post', '__return_false', 10);
 
 // disable for post types
 add_filter('use_block_editor_for_post_type', '__return_false', 10);
+
+// require
+
+require_once THEME_DIR . '/includes/theme-actions.php';
---
+++ includes/theme-actions.php
@@ -0,0 +1,27 @@
+<?php 
+
+if(!defined('ABSPATH'))
+    exit;
+
+
+    // Return formatted top-nav menu
+function top_nav_menu() {
+    $menu = wp_get_nav_menu_items('top-nav');
+    $result = [];
+    foreach($menu as $item) {
+        $my_item = [
+            'name' => $item->title,
+            'href' => $item->url
+        ];
+        $result[] = $my_item;
+    }
+    return $result;
+}
+// add endpoint
+add_action( 'rest_api_init', function() {
+    // top-nav menu
+    register_rest_route( 'wp/v2', 'top-nav', array(
+        'methods' => 'GET',
+        'callback' => 'top_nav_menu',
+    ) );
+});
