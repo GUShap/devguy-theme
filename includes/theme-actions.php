a907204d4b6103d9147423313ca514b73bd0bdd4
GUShap <shapira91@gmail.com>
2024-10-18 12:59:59 +0300

    Update theme-actions.php

--- includes/theme-actions.php
+++ includes/theme-actions.php
@@ -25,3 +25,16 @@ add_action( 'rest_api_init', function() {
         'callback' => 'top_nav_menu',
     ) );
 });
+
+//  support logo
+
+function theme_custom_logo_setup() {
+    add_theme_support('custom-logo', array(
+        'height'      => 100,     // Set desired logo height
+        'width'       => 400,     // Set desired logo width
+        'flex-height' => true,    // Allow flexible height
+        'flex-width'  => true,    // Allow flexible width
+        'header-text' => array('site-title', 'site-description'), // Header text you want to display
+    ));
+}
+add_action('after_setup_theme', 'theme_custom_logo_setup');
